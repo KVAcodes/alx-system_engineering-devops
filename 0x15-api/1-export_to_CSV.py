d2fb54df17bbc94376a72d1e754187bb71fd5634
KVAcodes <kadiriavictor@gmail.com>
2023-10-01 21:00:29 +0000

    added task 1

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Using what I did in task #0 I'm using this script to export data
+in the CSV format.
+"""
+import csv
+import json
+import requests
+import sys
+
+
+if __name__ == '__main__':
+    id = sys.argv[1]
+    with (
+        requests.get(f'https://jsonplaceholder.typicode.com/users/{id}')
+    ) as user_json:
+        with (
+            requests.get(
+                f'https://jsonplaceholder.typicode.com/users/{id}/todos'
+            )
+        ) as todos_json:
+            user = json.loads(user_json.text)
+            todos = json.loads(todos_json.text)
+            with open(f"{id}.csv", 'w', newline='') as csv_file:
+                csv_writer = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
+                for todo in todos:
+                    row_values = [f"{id}", f"{user['name']}",
+                                  f"{todo['completed']}", f"{todo['title']}"]
+                    csv_writer.writerow(row_values)
